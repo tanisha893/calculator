7593423c835109e0bc1a89d702af172fa4a9d808
tanisha893 <115476575+tanisha893@users.noreply.github.com>
2022-10-10 19:22:24 +0530

    project

---
+++ script.js
@@ -0,0 +1,35 @@
+let bracketflag = 0;
+    const addtodisplay = (myval) => {
+        let display = document.getElementById("display");
+        if(myval == "C"){
+            display.value = "";
+            bracketflag = 0;
+        }
+
+        else if(myval == "Back"){
+            display.value = display.value.slice(0,-1);
+        }
+
+        else if(myval == "()") {
+            if (bracketflag == 0) {
+                display.value += "(";
+                bracketflag = 1;
+            }
+            else if( bracketflag == 1) {
+                display.value += ")";
+                bracketflag = 0;
+            }
+        }
+        else if (myval == "="){
+            try{
+                display.value = eval(display.value);
+            }
+            catch{
+                display.value = "Invalid format";
+            }
+        }
+
+        else{
+            display.value += myval ;
+        }
+    }
